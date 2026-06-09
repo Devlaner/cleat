c3372e15950c856e9eb129c503c5927bda893a90
martian56 <fuadelizade469@gmail.com>
2026-06-09 10:52:35 +0400

    chore(backend): add the persistence module build file

---
+++ backend/libs/persistence/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api(project(":libs:domain"))
+    implementation(project(":libs:common"))
+
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
