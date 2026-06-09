40c9250f1e32ac42be11ec0fc16ed5a40633481e
martian56 <fuadelizade469@gmail.com>
2026-06-09 10:52:36 +0400

    chore(backend): add the scanning module build file

---
+++ backend/libs/scanning/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api(project(":libs:domain"))
+    implementation(project(":libs:common"))
+    implementation(project(":libs:persistence"))
+    implementation(project(":libs:github-client"))
+
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
