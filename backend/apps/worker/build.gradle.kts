bc56715a4c7bb6a484f9768e1dc462262f7c1d58
martian56 <fuadelizade469@gmail.com>
2026-06-09 10:52:31 +0400

    chore(backend): add the worker module build file

---
+++ backend/apps/worker/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("org.springframework.boot")
+}
+
+dependencies {
+    implementation(project(":libs:common"))
+    implementation(project(":libs:domain"))
+    implementation(project(":libs:persistence"))
+    implementation(project(":libs:github-client"))
+    implementation(project(":libs:enrichment"))
+    implementation(project(":libs:scanning"))
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
