9b41c23a74599a1dca931b9d466a37c16a989f91
Arzu <arzunusretova12@gmail.com>
2026-06-09 16:35:27 +0400

    fix: remove persistence changes

--- backend/libs/persistence/build.gradle.kts
+++ backend/libs/persistence/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
-    testImplementation("org.testcontainers:junit-jupiter")
+
     runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
