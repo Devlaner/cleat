28a4c0d52130623531751b4a22060d7164edacd2
martian56 <fuadelizade469@gmail.com>
2026-06-09 10:52:33 +0400

    chore(backend): add the common module build file

---
+++ backend/libs/common/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api("org.springframework.boot:spring-boot-starter")
+    api("org.springframework.boot:spring-boot-starter-validation")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
