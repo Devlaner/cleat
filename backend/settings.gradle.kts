472bf835d5529afa617721149c4c2ce78023f794
martian56 <fuadelizade469@gmail.com>
2026-06-09 10:52:29 +0400

    chore(backend): register the modules in settings

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "cleat-backend"
+
+include("apps:api")
+include("apps:worker")
+
+include("libs:common")
+include("libs:domain")
+include("libs:persistence")
+include("libs:github-client")
+include("libs:enrichment")
+include("libs:scanning")
