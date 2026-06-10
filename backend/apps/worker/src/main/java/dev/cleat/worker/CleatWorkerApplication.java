6c94c7310e31729221cc39588e0f213939e734a9
nurlanakberli9-pixel <nakberli841@gmail.com>
2026-06-10 00:45:26 -0700

    chore(worker): exclude DataSource and Redis auto-configurations

--- backend/apps/worker/src/main/java/dev/cleat/worker/CleatWorkerApplication.java
+++ backend/apps/worker/src/main/java/dev/cleat/worker/CleatWorkerApplication.java
@@ -1,4 +1,20 @@
 package dev.cleat.worker;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        RedisAutoConfiguration.class
+})
+@EnableScheduling
 public class CleatWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleatWorkerApplication.class, args);
+        String s="jjq";
+    }
 }
