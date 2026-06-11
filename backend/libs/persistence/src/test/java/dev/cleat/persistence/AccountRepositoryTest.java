a4161249cb6f6b092c7e563bc094c018a6faac0f
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-11 21:49:55 +0400

    style: apply spotless code formatting

--- backend/libs/persistence/src/test/java/dev/cleat/persistence/AccountRepositoryTest.java
+++ backend/libs/persistence/src/test/java/dev/cleat/persistence/AccountRepositoryTest.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
--- backend/libs/persistence/src/test/java/dev/cleat/persistence/PersistenceTestConfig.java
+++ backend/libs/persistence/src/test/java/dev/cleat/persistence/PersistenceTestConfig.java
@@ -1,6 +1,5 @@
 package dev.cleat.persistence;
 
-
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,5 +7,4 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration // @SpringBootConfiguration əvəzinə
 @EnableJpaRepositories(basePackages = "dev.cleat.persistence")
 @EntityScan(basePackages = "dev.cleat.persistence")
-public class PersistenceTestConfig {
-}
+public class PersistenceTestConfig {}
