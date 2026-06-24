40bf4a82cb7cfccbcc13ff832330b243f803a691
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-24 22:00:50 +0400

    fix:update api test configuration to resolve Docker initialization errors

--- backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
+++ backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
@@ -1,9 +1,13 @@
 package dev.cleat.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
+@EntityScan(basePackages = "dev.cleat.persistence.entity")
+@EnableJpaRepositories(basePackages = "dev.cleat.persistence.repository")
 public class CleatApiTests extends AbstractIntegrationTest {
 
     @Test
