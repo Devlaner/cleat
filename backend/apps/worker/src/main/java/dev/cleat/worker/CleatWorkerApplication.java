dfa24515c90b537c287fcd8547f598688910aa64
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-29 21:13:50 +0400

    fix(spring-context): add @EntityScan and @EnableJpaRepositories to CleatWorkerApplication in worker module

--- backend/apps/worker/src/main/java/dev/cleat/worker/CleatWorkerApplication.java
+++ backend/apps/worker/src/main/java/dev/cleat/worker/CleatWorkerApplication.java
@@ -2,6 +2,8 @@ package dev.cleat.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(
@@ -12,6 +14,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
             "dev.cleat.domain",
             "dev.cleat.common"
         })
+@EnableJpaRepositories(basePackages = "dev.cleat.persistence.repository")
+@EntityScan(basePackages = "dev.cleat.persistence.entity")
 @EnableScheduling
 public class CleatWorkerApplication {
 
