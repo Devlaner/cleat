e44793761c0454c96998ea60d9e5aab88954879e
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-29 14:45:46 +0400

    test: remove redundant application-test.properties configurations to rely on @ServiceConnection

--- backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
+++ backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
@@ -1,13 +1,8 @@
 package dev.cleat.api;
 
-import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = CleatApiApplication.class)
@@ -17,16 +12,4 @@ public class CleatApiTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoad() {}
-
-    @TestConfiguration
-    static class TestConfig {
-        @Primary
-        @Bean
-        public DataSource dataSource() {
-            return DataSourceBuilder.create()
-                    .url("jdbc:tc:postgresql:16-alpine:///cleat_db")
-                    .driverClassName("org.testcontainers.jdbc.ContainerDatabaseDriver")
-                    .build();
-        }
-    }
 }
