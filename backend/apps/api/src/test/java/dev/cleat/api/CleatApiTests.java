b8d0b5da943cf02604d9b96e7798fea5d4cc83e7
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-29 17:36:56 +0400

    test: isolate test contexts with dedicated  TestConfigs

--- backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
+++ backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
@@ -3,14 +3,9 @@ package dev.cleat.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
-@SpringBootTest(classes = CleatApiApplication.class, properties = "spring.main.allow-bean-definition-overriding=true")
+@SpringBootTest(classes = TestApiConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ComponentScan(
-        basePackages = "dev.cleat",
-        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "dev.cleat.worker.*"))
 public class CleatApiTests extends AbstractIntegrationTest {
 
     @Test
--- backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
+++ backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
@@ -3,14 +3,9 @@ package dev.cleat.worker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 
-@SpringBootTest(classes = CleatWorkerApplication.class)
+@SpringBootTest(classes = TestWorkerConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ComponentScan(
-        basePackages = "dev.cleat",
-        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "dev.cleat.api.*"))
 public class CleatWorkerApplicationTests extends AbstractIntegrationTest {
 
     @Test
