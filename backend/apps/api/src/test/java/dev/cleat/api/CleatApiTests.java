13430018ced0179350b9e76f99f0c2936cefcaa9
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-29 15:30:50 +0400

    test: remove @EntityScan and @EnableJpaRepositories from integration tests

--- backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
+++ backend/apps/api/src/test/java/dev/cleat/api/CleatApiTests.java
@@ -1,15 +1,11 @@
 package dev.cleat.api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest(classes = CleatApiApplication.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@EnableJpaRepositories(basePackages = "dev.cleat")
-@EntityScan(basePackages = "dev.cleat")
 public class CleatApiTests extends AbstractIntegrationTest {
 
     @Test
--- backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
+++ backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
@@ -1,15 +1,11 @@
 package dev.cleat.worker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest(classes = CleatWorkerApplication.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@EnableJpaRepositories(basePackages = "dev.cleat")
-@EntityScan(basePackages = "dev.cleat")
 public class CleatWorkerApplicationTests extends AbstractIntegrationTest {
 
     @Test
