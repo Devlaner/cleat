288381f1aa20a4e2a450d1ea3e59b6b1b4386fc7
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-29 14:34:19 +0400

    test: resolve NoUniqueBeanDefinitionException by providing primary datasource in test configuration

--- backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
+++ backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
@@ -1,8 +1,13 @@
 package dev.cleat.worker;
 
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = CleatWorkerApplication.class)
@@ -12,4 +17,16 @@ public class CleatWorkerApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {}
+
+    @TestConfiguration
+    static class TestConfig {
+        @Primary
+        @Bean
+        public DataSource dataSource() {
+            return DataSourceBuilder.create()
+                    .url("jdbc:tc:postgresql:16-alpine:///cleat_db")
+                    .driverClassName("org.testcontainers.jdbc.ContainerDatabaseDriver")
+                    .build();
+        }
+    }
 }
