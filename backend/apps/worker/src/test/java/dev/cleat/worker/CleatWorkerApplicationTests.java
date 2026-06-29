6d6949bebfeef6097358b96f35d2dc8b29688567
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-29 14:56:34 +0400

    test: add @EnableJpaRepository and @EntityScan to fix multi-module bean discovery

--- backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
+++ backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
@@ -1,13 +1,15 @@
 package dev.cleat.worker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest(classes = CleatWorkerApplication.class)
-@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@EnableJpaRepositories(basePackages = "dev.cleat")
+@EntityScan(basePackages = "dev.cleat")
 public class CleatWorkerApplicationTests extends AbstractIntegrationTest {
 
     @Test
