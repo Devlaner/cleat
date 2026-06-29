b71a4c5c16290560d7c8422d31d344a1232571ef
arzunusretova12-debug <arzunusretova12@gmail.com>
2026-06-29 15:38:05 +0400

    test: add @EntityScan and @EnableJpaRepositories to CleatWorkerApllicationTests

--- backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
+++ backend/apps/worker/src/test/java/dev/cleat/worker/CleatWorkerApplicationTests.java
@@ -1,11 +1,15 @@
 package dev.cleat.worker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest(classes = CleatWorkerApplication.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@EntityScan(basePackages = "dev.cleat.persistence.entity")
+@EnableJpaRepositories(basePackages = "dev.cleat.persistence.repository")
 public class CleatWorkerApplicationTests extends AbstractIntegrationTest {
 
     @Test
